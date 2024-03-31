30f866cad5ce3bfca952a2d837a9524157b25df9
init-AK <akshath1015@gmail.com>
2024-03-31 18:24:22 -0500

    server changes

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,14 @@
 const http = require('http')
 
+require('dotenv').config()
+
 const app = require('./app')
 const { mongoConnect } = require('./services/mongo')
 
 const { loadPlanetsData } = require('./models/planets.model')
 const { loadLaunchData } = require('./models/launches.model')
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT 
 
 const server = http.createServer(app)
 
--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const MONGO_URL = `mongodb+srv://akshath1015:HmZFhRsT6dtMybA3@democluster.mbabkkk.mongodb.net/nasa?retryWrites=true&w=majority&appName=DemoCluster`
+const MONGO_URL = process.env.MONGO_URL
 
 mongoose.connection.once('open', () => {
     console.log('MongoDB Connection ready!')
